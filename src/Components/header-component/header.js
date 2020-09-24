b03122e5de8c2a966f2da061f1835b1b18ae0e82
imadcollin <imad.collin@gmail.com>
2020-09-24 11:21:57 +0200

    Replace datetime with momentjs

--- src/Components/header-component/header.js
+++ src/Components/header-component/header.js
@@ -1,15 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./header.css";
-class Header extends Component {
-	
-	render() { 
-		return ( 
-			<div className="header-style">
-				<h1>This is a header</h1>
-			</div> 
+import Moment from "react-moment";
+import Container from '@material-ui/core/Container';
 
-		);
-	}
+class Header extends Component {
+  render() {
+    const date = new Date();
+
+    return (
+      <div className="header-style">
+        <h1>This is a header</h1>
+
+        <br />
+        <Container maxWidth="sm">
+
+          <h1 style={{ color: "black", backgroundColor: "#fff" }}>test</h1>
+
+          <span className="today">
+            <Moment format="dddd">{this.props.dateToFormat}</Moment>
+          </span>
+          <span className="day">
+            <Moment format="DD">{this.props.dateToFormat}</Moment>
+          </span>
+          <span className="month">
+            <Moment format="MMM">{this.props.dateToFormat}</Moment>
+          </span>
+          <span className="year">
+            <Moment format="YY">{this.props.dateToFormat}</Moment>
+          </span>
+        </Container>
+      </div>
+    );
+  }
 }
- 
+
 export default Header;
