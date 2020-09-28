8ce4be3c52e3ad5a7ad19f1ef8fa9d8abcf1c60a
imadcollin <imad.collin@gmail.com>
2020-09-28 11:23:24 +0200

    Add date to header

--- src/Components/header-component/header.js
+++ src/Components/header-component/header.js
@@ -1,37 +1,68 @@
 import React, { Component } from "react";
 import "./header.css";
 import Moment from "react-moment";
-import Container from '@material-ui/core/Container';
+import Container from "@material-ui/core/Container";
 
-class Header extends Component {
-  render() {
-    const date = new Date();
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
-    return (
-      <div className="header-style">
-        <h1>This is a header</h1>
+import Typography from "@material-ui/core/Typography";
 
-        <br />
-        <Container maxWidth="sm">
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0px",
+    transform: "scale(0.8)",
+  },
+}));
 
-          <h1 style={{ color: "black", backgroundColor: "#fff" }}>test</h1>
+export default function Header() {
+  const classes = useStyles();
+  const date = new Date();
 
-          <span className="today">
-            <Moment format="dddd">{this.props.dateToFormat}</Moment>
-          </span>
-          <span className="day">
-            <Moment format="DD">{this.props.dateToFormat}</Moment>
-          </span>
-          <span className="month">
-            <Moment format="MMM">{this.props.dateToFormat}</Moment>
-          </span>
-          <span className="year">
-            <Moment format="YY">{this.props.dateToFormat}</Moment>
-          </span>
-        </Container>
-      </div>
-    );
-  }
+  return (
+    <div className="header-style">
+      <h1>This is a header</h1>
+
+      <br />
+      <Container maxWidth="sm">
+        <h2>Grid container</h2>
+        <Grid container>
+          <Grid item xs={2}>
+            <span
+              className="day"
+              style={{
+                textAlign: "right",
+                fontSize: "80px",
+                fontFamily: "bold",
+              }}
+            >
+              <Moment format="DD">{date}</Moment>
+            </span>
+          </Grid>
+
+          <Grid item xs={2}>
+            <Typography variant="h5" component="h1">
+              <span className="month">
+                <Moment format="MMM">{date}</Moment>
+              </span>
+            </Typography>
+
+            <h2 className="year">
+              <Moment format="YYYY">{date}</Moment>
+            </h2>
+          </Grid>
+
+          <Grid item xs={8}>
+            <h1 className="dd" style={{ lineHeight: "2.5" }}>
+              <Moment format="dd">{date}</Moment>
+            </h1>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
-
-export default Header;
