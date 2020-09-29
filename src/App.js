6dc45064031cadc299e8da2ffa8c544dea815bcd
imadcollin <imad.collin@gmail.com>
2020-09-29 09:27:08 +0200

    Initial functionality set up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import  Header  from "./Components/header-component/header";
-import './App.css';
+import React, { useState } from "react";
+import Header from "./Components/header-component/Header-component";
+import Add from "./Components/header-component/AddItem-component";
+import TodoList from "./Components/header-component/TodoList-component";
+import "./App.css";
 
-function App() {
+const App = () => {
+  let randomId = Math.floor(Math.random() * 10 + 1);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Breakfast",
+    },
+    { id: 2, title: "Dinner" },
+  ]);
+  const deleteItem = (id) => {
+    setTodos((prev) => {
+      return prev.filter((prev) => prev.id !== id);
+    });
+  };
+  const addItem = ({ text }) => {
+    if (!text) {
+      alert("Please type something... ");
+    } else {
+      setTodos((prev) => {
+        return [{ id: randomId, title: text }, ...prev];
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">      
-        <h1>Test Project</h1>
-      </header>
+      <header className="App-header"></header>
       <Header></Header>
+      <Add items={todos} addItem={addItem}></Add>
+      {todos.map((item) => (
+        <TodoList item={item} deleteItem={deleteItem}></TodoList>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
