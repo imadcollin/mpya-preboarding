2c06feb1482564e9adc3c291d864b569d3848fce
imadcollin <imad.collin@gmail.com>
2020-10-05 15:29:22 +0200

    Add material icons

--- src/Components/header-component/TodoList-component.js
+++ src/Components/header-component/TodoList-component.js
@@ -1,19 +1,43 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import CancelIcon from "@material-ui/icons/Cancel";
+import EditIcon from "@material-ui/icons/Edit";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 const TodoList = ({ item, deleteItem, update, index }) => {
+  const classes = useStyles();
+
   return (
-    <div style={{ backgroundColor: "grey" }}>
-      <header className="App-header">
-        <ul>
-          <li>
-  {item.id} - {item.title} -- {item.creator}---{" "}
-            <button onClick={() => deleteItem(item.id)}>Delete</button>
-            <button onClick={() => update(item.id, item.title, item.creator, index)}>
-              Update
-            </button>
-          </li>
-        </ul>
-      </header>
+    <div>
+      <List component="nav" className={classes.root} aria-label="contacts">
+        <ListItem button>
+          <ListItemText primary={item.title} secondary={item.creator} />{" "}
+          <EditIcon
+            style={{
+              color: "black",
+              backgroundColor: "white",
+              fontSize: "2rem",
+            }}
+            onClick={() => update(item.id, item.title, item.creator, index)}
+          ></EditIcon>
+          <span>&nbsp;&nbsp;</span>
+          <CancelIcon
+            style={{
+              color: "white",
+              backgroundColor: "red",
+              borderRadius: "50%",
+            }}
+            onClick={() => deleteItem(item.id)}
+          ></CancelIcon>
+        </ListItem>
+      </List>
     </div>
   );
 };
